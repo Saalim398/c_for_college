edb93f0575b67c52e6427b09e9d257e770e878dd
Mohammad Saalim Khan <137300854+Saalim398@users.noreply.github.com>
2024-09-26 09:04:30 +0530

    Update 59.c

--- 59.c
+++ 59.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
+    if (n == 1 or n==0) {
+        return 1;
     } else {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
